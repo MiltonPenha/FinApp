fbc7e3f4fa65ec5552880fadbeb9ff75a2b55e8d
Milton Penha <miltonjmiltonj@gmail.com>
2025-04-06 18:55:08 -0300

    feat: Add user authentication using Clerk

---
+++ frontend/src/app/login/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function CreateAccountPage() {
+  return (
+    <div className='h-screen flex items-center justify-center flex-col gap-4'>
+        <SignIn />
+    </div>
+  )
+}
